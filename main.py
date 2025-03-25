b5a0ab96930b672d843deb65875ff12cc63e958b
Mustafa Bolat <mooshcor@outlook.com>
2025-03-25 14:08:04 -0400

    Adding SQLite3

--- main.py
+++ main.py
@@ -2,6 +2,32 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from routers.chat import router
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field()
+    content: str = Field()
+    url: str | None = Field(default=None)
+    isContent: bool = Field(default=True)
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 
 app = FastAPI()
 
